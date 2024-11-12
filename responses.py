d6195e7669186b7984f1e78db06f1c19e8b5f020
Konne <50216303+KonneBOT@users.noreply.github.com>
2024-11-12 11:37:43 +0100

    Edited VDI und constant Responses

--- responses.py
+++ responses.py
@@ -3,13 +3,13 @@ from random import choice, randint
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
-    if lowered == '':
-        return "Hmm, you quiet aren't you?"
+    if lowered == 'asdf':
+        choice(['I do not understand.', 'I am not sure what you mean.', 'Could you rephrase that?'])
     elif 'hello' in lowered:
         return "Hello there!"
     elif 'bye' in lowered:
         return "Goodbye!"
+    elif 'hower' in lowered:
+        return choice(['Der VDI wird Sie nicht retten!', 'Ja ne, das ist falsch', 'Denkste das ist richtig?', "Sehen und Hören Sie mich?"])
     elif 'roll' in lowered:
         return f"You rolled a {randint(1, 6)}."
-    else:
-        return choice(['I do not understand.', 'I am not sure what you mean.', 'Could you rephrase that?'])
