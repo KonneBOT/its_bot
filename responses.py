341cd508d26b8554f30455a7b3f5a794dd4d5769
Konne <50216303+KonneBOT@users.noreply.github.com>
2024-11-13 11:33:43 +0100

    more Hower = good

--- responses.py
+++ responses.py
@@ -12,6 +12,8 @@ def get_response(user_input: str) -> str:
     elif 'bye' in lowered:
         return "Goodbye!"
     elif 'hower' in lowered:
-        return choice(['Der VDI wird Sie nicht retten!', 'Ja ne, das ist falsch', 'Denkste das ist richtig?', "Sehen und Hören Sie mich?"])
+        return choice(['Der VDI wird Sie nicht retten!', 'Ja ne, das ist falsch', 'Denkste das ist richtig?', 
+                       "Sehen und Hören Sie mich?", "Was mein Sie dahinten im Lovechair?", "Grüße an das Reha Zentrum!",
+                       "Sie kommen nie in den 05er!", "Dreimal durch die Brust geschossen!"])
     elif 'roll' in lowered and not "rollen" in lowered:
         return f"You rolled a {randint(1, 6)}."
