cdee984251be5c83506e685cfa5d6f07794a3e84
Konne <50216303+KonneBOT@users.noreply.github.com>
2024-11-16 17:16:35 +0100

    Smoll change

--- responses.py
+++ responses.py
@@ -6,16 +6,16 @@ def get_response(user_input: str) -> str:
     if lowered == "":
         return
     elif lowered == 'asdf':
-        choice(['I do not understand.', 'I am not sure what you mean.', 'Could you rephrase that?'])
+        return choice(['I do not understand.', 'I am not sure what you mean.', 'Could you rephrase that?'])
     elif 'hello there' in lowered:
         return "General Kenobi!"
     elif 'hello' in lowered:
         return "Hello there!"
     elif 'bye' in lowered:
         return "Goodbye!"
-    elif 'hower' in lowered:
+    elif ' hower' in lowered:
         return choice(['Der VDI wird Sie nicht retten!', 'Ja ne, das ist falsch', 'Denkste das ist richtig?', 
-                       "Sehen und Hören Sie mich?", "Was mein Sie dahinten im Lovechair?", "Grüße an das Reha Zentrum!",
+                       "Sehen und Hören Sie mich?", "Was meinen Sie dahinten im Lovechair?", "Grüße an das Reha Zentrum!",
                        "Sie kommen nie in den 05er!", "Dreimal durch die Brust geschossen!"])
     elif 'roll' in lowered and not "rollen" in lowered:
         return f"You rolled a {randint(1, 6)}."
