bf9b48e099674d793711442b5c6dbba74d910fbf
Konne <50216303+KonneBOT@users.noreply.github.com>
2024-12-20 12:08:35 +0100

    Apfelmus

--- responses.py
+++ responses.py
@@ -17,6 +17,6 @@ def get_response(user_input: str) -> str:
         return choice(['Der VDI wird Sie nicht retten!', 'Ja ne, das ist falsch', 'Denkste das ist richtig?', 
                        "Sehen und Hören Sie mich?", "Was mein Sie dahinten im Lovechair?", "Grüße an das Reha Zentrum!",
                        "Sie kommen nie in den 05er!", "Dreimal durch die Brust geschossen!", "Das ist der gängige Fehler!",
-                       "Das brauchen Sie sich nicht auf die Sprudelflasche schreiben"])
+                       "Das brauchen Sie sich nicht auf die Sprudelflasche schreiben", "Danach ist Apfelmus!"])
     elif 'roll' in lowered and not "rollen" in lowered:
         return f"You rolled a {randint(1, 6)}."
