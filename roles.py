130709da14343fd718ff2b30207f6ce07a77148c
Meta <faul.dani@gmail.com>
2025-03-11 15:10:44 +0100

    replaced ["ITS", "TI", "WIN"] with courses

--- roles.py
+++ roles.py
@@ -5,13 +5,9 @@ courses = ["ITS", "TI", "WIN", "DCT", "WIW"]
 async def update_sem_roles(member: discord.Member) -> None:
     role_maps = []
     for i in range(1, 8):
-        role_map = {
-            "ITS": f'{i}. Sem - ITS',
-            "TI": f'{i}. Sem - TI',
-            "WIN": f'{i}. Sem - WIN',
-            "DCT": f'{i}. Sem - DCT',
-            "WIW": f'{i}. Sem - WIW',
-        }
+        role_map = {}
+        for course in courses:
+            role_map[course] = f'{i}. Sem - {course}'
         role_maps.append(role_map)
 
     roles = [role.name for role in member.roles]
@@ -56,7 +52,7 @@ async def remove_old_sem_roles(member: discord.Member, old_roles: list) -> None:
     
     rmvd_role = [role.name for role in old_roles if role not in member.roles][0]           # nur noch die gelöschte Rolle bleibt übrig
     sem_numbers = [int(role.name[-1]) for role in member.roles if role.name[-1].isdigit()]       # Semesterzahlen extrahieren
-    studiengaenge = [role.name for role in member.roles if role.name in ["ITS", "TI", "WIN"]]   # Studiengänge extrahieren
+    studiengaenge = [role.name for role in member.roles if role.name in courses]   # Studiengänge extrahieren
 
     print(f"Removed role: {rmvd_role}")
 
@@ -66,7 +62,7 @@ async def remove_old_sem_roles(member: discord.Member, old_roles: list) -> None:
             await member.remove_roles(discord.utils.get(member.guild.roles, name=f"{sem}. Sem - {studiengang}"))
             print(f"Removed role '{sem}. Sem - {studiengang}' from {member.name}")
 
-    elif rmvd_role in ["ITS", "TI", "WIN"]: # Wenn die gelöschte Rolle ein Studiengang war
+    elif rmvd_role in courses: # Wenn die gelöschte Rolle ein Studiengang war
         for sem in sem_numbers:
             await member.remove_roles(discord.utils.get(member.guild.roles, name=f"{sem}. Sem - {rmvd_role}"))
             print(f"Removed role '{sem}. Sem - {rmvd_role}' from {member.name}")
