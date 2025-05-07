cfe1fa4df8bb29cd3255fd6fc03d4fc9a6a563d9
KonneBOT <50216303+KonneBOT@users.noreply.github.com>
2025-05-07 08:19:21 +0200

    Add new Responses

--- responses.py
+++ responses.py
@@ -18,6 +18,7 @@ def get_response(user_input: str) -> str:
                        "Sehen und Hören Sie mich?", "Was mein Sie dahinten im Lovechair?", "Grüße an das Reha Zentrum!",
                        "Sie kommen nie in den 05er!", "Dreimal durch die Brust geschossen!", "Das ist der gängige Fehler!",
                        "Das brauchen Sie sich nicht auf die Sprudelflasche schreiben", "Danach ist Apfelmus!", 
-                       "Danach ist Französische Revolution!", "Wenn der Index außerhalb der Liste ist, fährt ja der Computer runter 🫠"])
+                       "Danach ist Französische Revolution!", "Wenn der Index außerhalb der Liste ist, fährt ja der Computer runter 🫠",
+                       "Also man kann es ja verkacken, aber so???", "Jetzt mach ich erstmal Sie fertig!"])
     elif 'roll' in lowered and not "rollen" in lowered:
         return f"You rolled a {randint(1, 6)}."
