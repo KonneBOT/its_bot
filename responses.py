58e1d40b1a912a702b6a205a1593a1c691c40e95
Pfanne <faul.dani@gmail.com>
2025-09-30 00:05:11 +0200

    #1 done

--- responses.py
+++ responses.py
@@ -1,24 +1,26 @@
 from random import choice, randint
+import codecs
+f = codecs.open("zitate.md", "r", "utf-8")
+quotes = f.readlines()[2:]
 
 def get_response(user_input: str) -> str:
+
     lowered: str = user_input.lower()
 
-    if lowered == "":
-        return
-    elif lowered == 'asdf':
-        choice(['I do not understand.', 'I am not sure what you mean.', 'Could you rephrase that?'])
-    elif 'hello there' in lowered:
-        return "General Kenobi!"
-    elif 'hello' in lowered:
-        return "Hello there!"
-    elif 'bye' in lowered:
-        return "Goodbye!"
-    elif 'hower' in lowered:
-        return choice(['Der VDI wird Sie nicht retten!', 'Ja ne, das ist falsch', 'Denkste das ist richtig?', 
-                       "Sehen und Hören Sie mich?", "Was mein Sie dahinten im Lovechair?", "Grüße an das Reha Zentrum!",
-                       "Sie kommen nie in den 05er!", "Dreimal durch die Brust geschossen!", "Das ist der gängige Fehler!",
-                       "Das brauchen Sie sich nicht auf die Sprudelflasche schreiben", "Danach ist Apfelmus!", 
-                       "Danach ist Französische Revolution!", "Wenn der Index außerhalb der Liste ist, fährt ja der Computer runter 🫠",
-                       "Also man kann es ja verkacken, aber so???", "Jetzt mach ich erstmal Sie fertig!"])
-    elif 'roll' in lowered and not "rollen" in lowered:
-        return f"You rolled a {randint(1, 6)}."
+    match True:
+        case _ if lowered == "":
+            return None
+        case _ if lowered == "asdf":
+            return choice(['I do not understand.', 'I am not sure what you mean.', 'Could you rephrase that?'])
+        case _ if "hello there" in lowered:
+            return "General Kenobi!"
+        case _ if "hello" in lowered:
+            return "Hello there!"
+        case _ if "bye" in lowered:
+            return "Goodbye!"
+        case _ if "hower" in lowered:
+            return choice(quotes).strip("\r\n")
+        case _ if "roll" in lowered and "rollen" not in lowered:
+            return f"You rolled a {randint(1, 6)}."
+        case _:
+            return None
---
+++ zitate.md
@@ -0,0 +1,17 @@
+### Zitate
+
+Der VDI wird Sie nicht retten!
+Ja ne, das ist falsch!
+Denkste das ist richtig?
+Sehen und Hören Sie mich?
+Was mein Sie dahinten im Lovechair?
+Grüße an das Reha Zentrum!
+Sie kommen nie in den 05er!
+Dreimal durch die Brust geschossen!
+Das ist der gängige Fehler!
+Das brauchen Sie sich nicht auf die Sprudelflasche schreiben!
+Danach ist Apfelmus!
+Danach ist Französische Revolution!
+Wenn der Index außerhalb der Liste ist, fährt ja der Computer runter 🫠
+Also man kann es ja verkacken, aber so???
+Jetzt mach ich erstmal Sie fertig!
